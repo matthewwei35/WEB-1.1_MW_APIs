f6f087c0c7b29ab5d9f43eac4a797907a324f39c
Meredith Murphy <meredithcat@gmail.com>
2020-08-29 20:58:24 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,213 @@
+import jinja2
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import pprint
+import requests
+import sqlite3
+
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, send_file
+from geopy.geocoders import Nominatim
+from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+
+################################################################################
+## SETUP
+################################################################################
+
+app = Flask(__name__)
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+# Settings for image endpoint
+matplotlib.use('agg')
+plt.style.use('ggplot')
+
+my_loader = jinja2.ChoiceLoader([
+    app.jinja_loader,
+    jinja2.FileSystemLoader('data'),
+])
+app.jinja_loader = my_loader
+
+
+################################################################################
+## ROUTES
+################################################################################
+
+@app.route('/')
+def home():
+    """Displays the homepage with forms for current or historical data."""
+    context = {
+        'min_date': (datetime.now() - timedelta(days=5)),
+        'max_date': datetime.now()
+    }
+    return render_template('home.html', **context)
+
+def get_letter_for_units(units):
+    """Returns a shorthand letter for the given units."""
+    return 'F' if units == 'imperial' else 'C' if units == 'metric' else 'K'
+
+@app.route('/results')
+def results():
+    """Displays results for current weather conditions."""
+    # TODO: Use 'request.args' to retrieve the city & units from the query
+    # parameters.
+    city = ''
+    units = ''
+
+    url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {
+        # TODO: Enter query parameters here for the 'appid' (your api key),
+        # the city, and the units (metric or imperial).
+        # See the documentation here: https://openweathermap.org/current
+
+    }
+    result_json = requests.get(url, params=params).json()
+
+    # Uncomment the line below to see the results of the API call!
+    # pp.pprint(result_json)
+
+    # TODO: Replace the empty variables below with their appropriate values.
+    # You'll need to retrieve these from the result_json object above.
+
+    # For the sunrise & sunset variables, I would recommend to turn them into
+    # datetime objects. You can do so using the `datetime.fromtimestamp()` 
+    # function.
+    context = {
+        'date': datetime.now(),
+        'city': '',
+        'description': '',
+        'temp': '',
+        'humidity': '',
+        'wind_speed': '',
+        'sunrise': '',
+        'sunset': '',
+        'units_letter': get_letter_for_units(units)
+    }
+
+    return render_template('results.html', **context)
+
+def get_min_temp(results):
+    """Returns the minimum temp for the given hourly weather objects."""
+    # TODO: Fill in this function to return the minimum temperature from the
+    # hourly weather data.
+    pass
+
+def get_max_temp(results):
+    """Returns the maximum temp for the given hourly weather objects."""
+    # TODO: Fill in this function to return the maximum temperature from the
+    # hourly weather data.
+    pass
+
+def get_lat_lon(city_name):
+    geolocator = Nominatim(user_agent='Weather Application')
+    location = geolocator.geocode(city_name)
+    if location is not None:
+        return location.latitude, location.longitude
+    return 0, 0
+
+
+@app.route('/historical_results')
+def historical_results():
+    """Displays historical weather forecast for a given day."""
+    city = ''
+    date = '2020-08-26'
+    units = ''
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+
+    latitude, longitude = get_lat_lon(city)
+
+    url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
+    params = {
+        'appid': API_KEY,
+        'lat': latitude,
+        'lon': longitude,
+        'units': units,
+        'dt': date_obj.strftime('%s')
+    }
+    result_json = requests.get(url, params=params).json()
+
+    # Uncomment the line below to see the results of the API call!
+    # pp.pprint(result_json)
+
+    result_current = result_json['current']
+    result_hourly = result_json['hourly']
+
+    # TODO: Replace the empty variables below with their appropriate values.
+    # You'll need to retrieve these from the `result_current` object above.
+    context = {
+        'city': '',
+        'date': date_obj,
+        'lat': latitude,
+        'lon': longitude,
+        'units': '',
+        'units_letter': '', # should be 'C', 'F', or 'K'
+        'description': '',
+        'temp': '',
+        'min_temp': get_min_temp(result_hourly),
+        'max_temp': get_min_temp(result_hourly)
+    }
+
+    return render_template('historical_results.html', **context)
+
+
+################################################################################
+## IMAGES
+################################################################################
+
+def create_image_file(xAxisData, yAxisData, xLabel, yLabel):
+    """
+    Creates and returns a line graph with the given data.
+    Written with help from http://dataviztalk.blogspot.com/2016/01/serving-matplotlib-plot-that-follows.html
+    """
+    fig, _ = plt.subplots()
+    plt.plot(xAxisData, yAxisData)
+    plt.xlabel(xLabel)
+    plt.ylabel(yLabel)
+    canvas = FigureCanvas(fig)
+    img = BytesIO()
+    fig.savefig(img)
+    img.seek(0)
+    return send_file(img, mimetype='image/png')
+
+@app.route('/graph/<lat>/<lon>/<units>/<date>')
+def graph(lat, lon, units, date):
+    """
+    Returns a line graph with data for the given location & date.
+    @param lat The latitude.
+    @param lon The longitude.
+    @param units The units (imperial, metric, or kelvin)
+    @param date The date, in the format %Y-%m-%d.
+    """
+    date_obj = datetime.strptime(date, '%Y-%m-%d')
+    date_epoch = date_obj.strftime('%s')
+
+    url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
+    params = {
+        'appid': API_KEY,
+        'lat': lat,
+        'lon': lon,
+        'units': units,
+        'dt': date_epoch
+    }
+    result_json = requests.get(url, params=params).json()
+
+    hour_results = result_json['hourly']
+
+    hours = range(24)
+    temps = [r['temp'] for r in hour_results]
+    image = create_image_file(
+        hours,
+        temps,
+        'Hour',
+        f'Temperature ({get_letter_for_units(units)})'
+    )
+    return image
+
+
+if __name__ == '__main__':
+    app.run()

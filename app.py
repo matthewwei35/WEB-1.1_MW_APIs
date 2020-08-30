88c92aa204570be21fdc3b28abe4e6917fb40747
Meredith Murphy <meredithcat@gmail.com>
2020-08-30 10:57:03 -0700

    made some changes to todos

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import os
 import pprint
+import pytz
 import requests
 import sqlite3
 
@@ -20,10 +21,13 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 app = Flask(__name__)
 
+# Get the API key from the '.env' file
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
+
 # Settings for image endpoint
+# Written with help from http://dataviztalk.blogspot.com/2016/01/serving-matplotlib-plot-that-follows.html
 matplotlib.use('agg')
 plt.style.use('ggplot')
 
@@ -66,6 +70,7 @@ def results():
         # See the documentation here: https://openweathermap.org/current
 
     }
+
     result_json = requests.get(url, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
@@ -114,21 +119,25 @@ def get_lat_lon(city_name):
 @app.route('/historical_results')
 def historical_results():
     """Displays historical weather forecast for a given day."""
+    # TODO: Use 'request.args' to retrieve the city & units from the query
+    # parameters.
     city = ''
     date = '2020-08-26'
     units = ''
     date_obj = datetime.strptime(date, '%Y-%m-%d')
+    date_in_seconds = date_obj.strftime('%s')
 
     latitude, longitude = get_lat_lon(city)
 
     url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
     params = {
-        'appid': API_KEY,
-        'lat': latitude,
-        'lon': longitude,
-        'units': units,
-        'dt': date_obj.strftime('%s')
+        # TODO: Enter query parameters here for the 'appid' (your api key),
+        # latitude, longitude, units, & date (in seconds).
+        # See the documentation here (scroll down to "Historical weather data"):
+        # https://openweathermap.org/api/one-call-api
+        
     }
+
     result_json = requests.get(url, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
@@ -138,7 +147,7 @@ def historical_results():
     result_hourly = result_json['hourly']
 
     # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the `result_current` object above.
+    # You'll need to retrieve these from the 'result_current' object above.
     context = {
         'city': '',
         'date': date_obj,
@@ -184,7 +193,8 @@ def graph(lat, lon, units, date):
     @param date The date, in the format %Y-%m-%d.
     """
     date_obj = datetime.strptime(date, '%Y-%m-%d')
-    date_epoch = date_obj.strftime('%s')
+    date_in_seconds = date_obj.strftime('%s')
+
 
     url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
     params = {
@@ -192,7 +202,7 @@ def graph(lat, lon, units, date):
         'lat': lat,
         'lon': lon,
         'units': units,
-        'dt': date_epoch
+        'dt': date_in_seconds
     }
     result_json = requests.get(url, params=params).json()
 

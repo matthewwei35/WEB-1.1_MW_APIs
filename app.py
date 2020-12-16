118e9d48943c5716ea5a9f7c7ed6b1e9840155ee
Meredith Murphy <meredithcat@gmail.com>
2020-12-15 22:14:26 -0800

    removed historical route, added comparison route

--- app.py
+++ app.py
@@ -16,9 +16,11 @@ app = Flask(__name__)
 
 # Get the API key from the '.env' file
 load_dotenv()
-API_KEY = os.getenv('API_KEY')
 
 pp = PrettyPrinter(indent=4)
+
+API_KEY = os.getenv('API_KEY')
+API_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 
 ################################################################################
@@ -46,7 +48,6 @@ def results():
     city = ''
     units = ''
 
-    url = 'http://api.openweathermap.org/data/2.5/weather'
     params = {
         # TODO: Enter query parameters here for the 'appid' (your api key),
         # the city, and the units (metric or imperial).
@@ -54,7 +55,7 @@ def results():
 
     }
 
-    result_json = requests.get(url, params=params).json()
+    result_json = requests.get(API_URL, params=params).json()
 
     # Uncomment the line below to see the results of the API call!
     # pp.pprint(result_json)
@@ -79,73 +80,31 @@ def results():
 
     return render_template('results.html', **context)
 
-def get_min_temp(results):
-    """Returns the minimum temp for the given hourly weather objects."""
-    # TODO: Fill in this function to return the minimum temperature from the
-    # hourly weather data.
-    pass
 
-def get_max_temp(results):
-    """Returns the maximum temp for the given hourly weather objects."""
-    # TODO: Fill in this function to return the maximum temperature from the
-    # hourly weather data.
-    pass
-
-def get_lat_lon(city_name):
-    geolocator = Nominatim(user_agent='Weather Application')
-    location = geolocator.geocode(city_name)
-    if location is not None:
-        return location.latitude, location.longitude
-    return 0, 0
-
-
-@app.route('/historical_results')
-def historical_results():
-    """Displays historical weather forecast for a given day."""
-    # TODO: Use 'request.args' to retrieve the city & units from the query
+@app.route('/comparison_results')
+def comparison_results():
+    """Displays the relative weather for 2 different cities."""
+    # TODO: Use 'request.args' to retrieve the cities & units from the query
     # parameters.
-    city = ''
-    date = '2020-08-26'
+    city1 = ''
+    city2 = ''
     units = ''
-    date_obj = datetime.strptime(date, '%Y-%m-%d')
-    date_in_seconds = date_obj.strftime('%s')
 
-    latitude, longitude = get_lat_lon(city)
+    # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
+    # helper function for this!
 
-    url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
-    params = {
-        # TODO: Enter query parameters here for the 'appid' (your api key),
-        # latitude, longitude, units, & date (in seconds).
-        # See the documentation here (scroll down to "Historical weather data"):
-        # https://openweathermap.org/api/one-call-api
-        
-    }
 
-    result_json = requests.get(url, params=params).json()
-
-    # Uncomment the line below to see the results of the API call!
-    # pp.pprint(result_json)
-
-    result_current = result_json['current']
-    result_hourly = result_json['hourly']
-
-    # TODO: Replace the empty variables below with their appropriate values.
-    # You'll need to retrieve these from the 'result_current' object above.
+    # TODO: Pass the information for both cities in the context. Make sure to
+    # pass info for the temperature, humidity, wind speed, and sunset time!
+    # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
+    # `city2_info`, to organize the data.
     context = {
-        'city': '',
-        'date': date_obj,
-        'lat': latitude,
-        'lon': longitude,
-        'units': '',
-        'units_letter': '', # should be 'C', 'F', or 'K'
-        'description': '',
-        'temp': '',
-        'min_temp': get_min_temp(result_hourly),
-        'max_temp': get_max_temp(result_hourly)
+
     }
 
-    return render_template('historical_results.html', **context)
+    return render_template('comparison_results.html', **context)
 
 
 if __name__ == '__main__':
+    app.config['ENV'] = 'development'
     app.run(debug=True)
